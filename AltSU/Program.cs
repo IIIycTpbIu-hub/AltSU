94a7b54558d80c94ab270e85e255b4d39dd82d6c
Andrew Raznopolov <razandrey95@gmail.com>
2020-11-02 16:17:31 +0400

    Добавил второе задание

--- AltSU/Program.cs
+++ AltSU/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AltSU.Task_1;
+using AltSU.Task_2;
 using System;
 
 namespace AltSU
@@ -12,6 +13,12 @@ namespace AltSU
             var empl2 = new Employee("Petr", "Petrov", "Petrovich", "Junior .Net developer", "Software development", 30000);
 
             Console.WriteLine("{0} {1}", empl1, empl2);
+
+            Console.WriteLine("Task_2");
+            var point1 = new Point();
+            var point2 = new Point(1, 2, 3);
+            var result = point1.DistanceTo(point2);
+            Console.WriteLine("Расстояние между двумя точками {0} и {1} равно: {2}", point1, point2, result);
         }
     }
 }
---
+++ AltSU/Task_2/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AltSU.Task_2
+{
+    public class Point
+    {
+        public double X { get; } = 0;
+        public double Y { get; } = 0;
+        public double Z { get; } = 0;
+
+        public Point(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Point()
+        {
+
+        }
+
+        public double DistanceTo(Point b)
+        {
+            return Math.Sqrt(Math.Pow((b.X - X), 2) + Math.Pow((b.Y - Y), 2) + Math.Pow((b.Z - Z), 2));
+        }
+
+        public override string ToString()
+        {
+            return "{x: " + X + ", y: " + Y + ", z:" + Z + "}";
+        }
+    }
+}
